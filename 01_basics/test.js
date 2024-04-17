81378b4443719a8b227e3e1810e284a2f0197c06
Payash Agarwal <31873627+payash007@users.noreply.github.com>
2024-04-17 10:04:51 +0000

    Datatype Conversion In JS

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,3 +1,62 @@
-console.log("payash")
+let accountCity ="Jhansi"
+let varInNumber= Number(accountCity)
+console.log(varInNumber)
+console.log(typeof varInNumber)
 
-//this is my first message
+
+/* Converting string value to a number will give NAN but the type will be a Number since type of NAN is a Number */
+
+let varInBoolean= Boolean(accountCity)
+console.log(varInBoolean)
+console.log(typeof varInBoolean)
+
+/* Converting a String to a Boolean Value will give true and the type will be boolean */
+//type of null is a object
+
+/*let accountNumber="33abc"
+let varInString= String(accountNumber)
+console.log(varInString)
+console.log(typeof varInString)*/
+
+let accountName=null
+//console.log(accountName)
+//console.log(typeof accountName)
+let accountNumber=Number(accountName)
+console.log(accountNumber)
+console.log(typeof accountNumber)
+
+/* If we convert null to a number it gives the value 0 and the type is a number */
+
+let score = undefined
+//console.log(score)
+//console.log(typeof score)
+let varInNumber1 =Number(score)
+let varInString = String(score)
+//console.log(varInNumber1)
+//console.log(typeof varInNumber1)
+
+console.log(varInString)
+console.log(typeof varInString)
+
+/* If we convert a undefined value to a number it gives NAN and type is a number 
+   If we convert a undefined value to a string it gives undefined and type is a string */
+
+let  value1= false
+console.log(value1)
+console.log(typeof value1)
+
+let varInString1= String(value1)
+console.log(varInString1)
+console.log(typeof varInString1)
+
+let varInNumber2 =Number(value1)
+console.log(varInNumber2)
+console.log(typeof varInNumber2)
+
+/* If we convert a Boolean value(true) to a string it gives true and type is string 
+   If we convert a Boolean value(true) to a number it gives 1 and type is a number */
+
+
+/*Converting a empty string to boolean will give false
+  Converting a non empty string to boolean will give true */
+
